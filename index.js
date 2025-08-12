a9d1ca1cde9a6f8073e8af84ad41a84accda8bec
Roberto Flores <25ASAB015@ujmd.edu.sv>
2025-08-11 18:21:50 -0600

    Refactor index.js to comment out variable logs and add interestRate constant logging

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // This is a comment
 // console.log("Hello World from JavaScript at index.js");
 
-let name = "Robert";
-console.log(name);
+//////////////////////
+// Variables
+//////////////////////
+// let name = "Robert";
+// console.log(name);
 
-let age = 20;
-console.log(age);
+// let age = 20;
+// console.log(age);
 
-let isStudent = true;
-console.log(isStudent);
+// let isStudent = true;
+// console.log(isStudent);
+
+//////////////////////
+// Constants
+//////////////////////
+const  interestRate = 0.3;
+console.log(interestRate);
