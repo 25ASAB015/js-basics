be25979a3cc82f266b488d735515b1dc46726257
Roberto Flores <25ASAB015@ujmd.edu.sv>
2025-08-11 18:14:50 -0600

    Update index.js to log variables for name, age, and isStudent, and comment out the original Hello World message

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // This is a comment
-console.log("Hello World from JavaScript at index.js");
+// console.log("Hello World from JavaScript at index.js");
+
+let name = "Robert";
+console.log(name);
+
+let age = 20;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
